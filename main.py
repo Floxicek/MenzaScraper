02a99701c57434bf9cfdd03979757ebf98862d00
Jan <jan@ejhle.net>
2024-11-20 14:28:04 +0100

    improve code and add notion_db

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from dotenv import load_dotenv
 import os, json
+import notion_db 
 
 load_dotenv()
 
@@ -19,22 +20,22 @@ def send_favorite_food():
     canteens_ids = []
     for c in config["canteens"]:
         canteens_ids.append(c["id"])
-    food = menza_scraper.get_daily_menu(canteens_ids)
     
     webhook = DiscordWebhook(url=os.getenv("WEBHOOK_URL"))
     
     embed = DiscordEmbed(title=f"{datetime.now().strftime("%c")}", color="AAFF00")
     embed.set_timestamp()
     is_empty = True
-    for i, place in enumerate(food):
-        for menu in place:
-            for f in menu:
-                # print(f)
-                for fa in config['food']:
-                    if fa in f:
-                        embed.add_embed_field(name=config["canteens"][i]["name"], value=f)
-                        is_empty = False
-                        break
+    
+    for cantine in canteens_ids:
+        menu = menza_scraper.get_daily_menu(cantine)
+        for food in menu:            
+            notion_db.add_food(f) # if food not in notion database, add it
+            for fa in config['food']:
+                if fa in f:
+                    embed.add_embed_field(name=config["canteens"][i]["name"], value=f)
+                    is_empty = False
+                    break
     if not is_empty:
         webhook.add_embed(embed)
         response = webhook.execute()
@@ -42,3 +43,4 @@ def send_favorite_food():
 
 if is_open():
     send_favorite_food()
+    
